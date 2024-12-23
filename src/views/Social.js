22fb562e99f9e4912d3a8e1dd7f9cc48822ab1ef
uxdsrini <uxdsrini@gmail.com>
2024-12-23 17:50:07 +0530

    favicon updated

--- src/views/Social.js
+++ src/views/Social.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PortfolioLinks = () => {
   return (
-    <div  className="bg-black lg:w-full lg:max-w-3xl lg:mx-auto px-2 py-2 md:flex text-white p-8 rounded-lg border border-lime-500 mt-1 justify-center gap-12 items-center mx-1.5 item-center">
+    <div  className="bg-black lg:w-full lg:max-w-3xl lg:mx-auto px-2 py-2 md:flex text-white p-8 rounded-lg border border-yellow-500 mt-1 justify-center gap-12 items-center mx-1.5 item-center">
       <div className="flex flex-col items-center text-center mt-6 mb-6">
         <div className="text-3xl ">
           <i className="fab fa-dribbble hover:bg-gradient-to-r from-purple-500 to-pink-500 hover:rounded-full p-2"></i>
