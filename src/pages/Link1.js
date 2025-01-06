bf684e03cc423ac8836c5df3286189938280776f
Soujanya0 <odelasoujanya2000@gmail.com>
2025-01-06 14:07:27 +0530

    Your commit message

---
+++ src/pages/Link1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Link1 = () => {
+  return <div>Hlo Content of Link1 </div>;
+};
+
+export default Link1;
---
+++ src/pages/Link3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Link3 = () => {
+  return <div>Content of Link3</div>;
+};
+
+export default Link3;
